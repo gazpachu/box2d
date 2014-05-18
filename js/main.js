d2afbdb4d4a48e8439135c9857490fd7813faea3
Joan Mira <joanmira@gmail.com>
2014-05-18 18:48:53 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function () {
+
+	window.requestAnimFrame = (function () {
+		return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback, element) {
+				window.setTimeout(callback, 1000 / 60);
+			};
+	})();
+
+	$(document).ready(function () {
+
+		
+	});
+
+})();
